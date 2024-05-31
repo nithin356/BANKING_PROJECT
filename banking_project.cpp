bea8dae57469ff3123d38d0c51c5f6f14d4b84d1
nithin356 <nithinbhandary356@gmail.com>
2024-05-31 15:46:20 +0530

    Added header function comments

---
+++ banking_project.cpp
@@ -0,0 +1,15 @@
+/*Header Files*/
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<vector>
+#include<map>
+/*Header Files*/
+
+using namespace std;
+
+/*Defining Minimum Balance*/
+#define MIN_BALANCE 500
+/*Nithin Component*/
+/*Gopi's Component*/
+/*Abhisek Component*/
