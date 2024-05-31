a9b62ad6829de439aa7054f72b63268e6b789112
nithin356 <nithinbhandary356@gmail.com>
2024-05-31 16:18:05 +0530

    Updated Minor Cahnges

--- banking_project.cpp
+++ banking_project.cpp
@@ -12,6 +12,9 @@ using namespace std;
 
 /*Defining Minimum Balance*/
 #define MIN_BALANCE 500
-/*Nithin Component*/
-/*Gopi's Component*/
-/*Abhisek Component*/
+/*Nithin Component Start*/
+/*Nithin Component End*/
+/*Gopi's Component Start*/
+/*Gopi's Component End*/
+/*Abhisek Component Start*/
+/*Abhisek Component End*/
